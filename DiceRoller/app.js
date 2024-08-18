2ddc3d7cd65c6aa93c709dd889223557f3dbc31b
Adnan Manik <adnanmanik0@gmail.com>
2024-08-18 22:57:57 +0500

    Making Dice Roller

---
+++ DiceRoller/app.js
@@ -0,0 +1,15 @@
+function rollDice() {
+    const numOfDices = document.getElementById("numOfDices").value;
+    const result = document.getElementById("result");
+    const resultImgs = document.getElementById("resultImgs");
+    let diceImgs=[];
+    let values=[];
+
+    for(let i=0; i < numOfDices;i++){
+        let value = Math.floor(Math.random() * 6 )+ 1;
+       values.push(value);
+       diceImgs.push(`<img src="images/${value}.png" >`)
+    }
+     console.log(values);
+}
+
